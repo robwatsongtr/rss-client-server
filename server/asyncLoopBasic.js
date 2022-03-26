d0f8c6c55053195c06356ef5d74e125e182ebc41
robwatsongtr <rwatso@gmail.com>
2022-03-25 21:15:11 -0700

    tidying up asyncLoopRSS.js

--- server/asyncLoopBasic.js
+++ server/asyncLoopBasic.js
@@ -1,36 +1,67 @@
-
+// example object 
 const fruitBasket = {
   apple: 27,
   grape: 0,
   pear: 14
 }
 
+// timer function that returns a promise 
 const timer = ms => {
   return new Promise( resolve => setTimeout(resolve, ms) )
 }
 
+// get the fruit! 
 const getNumFruit = f => {
   return timer(2000)
     .then ( v => fruitBasket[f] )
 }
 
-// getNumFruit('apple')
-//   .then( num => console.log(num) )
 
-const control = async _ => {
-  console.log('start')
 
-  const numApples = await getNumFruit('apple') 
-  console.log(numApples)
+// -----------await with map loop -------------
+// Essentially we create an array of promises in the map that then get 
+// resolved with promise.all 
 
-  const numGrapes = await getNumFruit('grape')
-  console.log(numGrapes)
+// what do we want to get from the object? 
+const fruitsToGet = ['apple', 'grape', 'pear']
 
-  const numPears = await getNumFruit('pear')
-  console.log(numPears)
+// outer async func: 
+const mapLoop = async () => {
 
-  console.log('End')
+  console.log('async in a map loop start: ');
+
+  // map loop 
+  const promises = fruitsToGet.map( async (fruit) => {
+    const numFruit = await getNumFruit(fruit)
+    return numFruit
+  })
+
+  // now we wait for the array of promises to get resolved:
+  const numFruits = await Promise.all(promises)
+
+  console.log( numFruits );
+  console.log( 'End' )
 
 }
 
-control()
+mapLoop()
+
+
+
+// //multiple hard-coded async calls in one async function example:
+// ( async () => {
+
+//   console.log('hard coded async calls: start')
+
+//   const numApples = await getNumFruit('apple') 
+//   console.log(numApples)
+
+//   const numGrapes = await getNumFruit('grape')
+//   console.log(numGrapes)
+
+//   const numPears = await getNumFruit('pear')
+//   console.log(numPears)
+
+//   console.log('End')
+
+// })()
--- server/asyncLoopRSS.js
+++ server/asyncLoopRSS.js
@@ -1,12 +1,12 @@
-// test 
 
+// Utilizing Async Await in a loop to return a bunch of stuff 
 
 const Parser = require('rss-parser');
 const parser = new Parser();
 
 const feedURls = ['https://jewishcurrents.org/partner.xml', 
   'https://readpassage.com/feed/', 
-  'http://popula.com/feed/', 
+  'http://popula.com/feed/',  
   'https://riftmag.org/interviews?format=rss', 
   'https://lbo-news.com/feed/',
   'https://ctmirror.org/feed/?partner-feed=optout', 
@@ -17,14 +17,25 @@ const feedURls = ['https://jewishcurrents.org/partner.xml',
   'https://thelaborreport.substack.com/feed'
 ]
 
-(async () => {
+// await with map loop -
+// We create an array of promises in the map that then get 
+// resolved with promise.all 
 
-  let feed = await parser.parseURL('https://www.reddit.com/.rss');
-  console.log(feed.title);
+const feedsLoop = async () => {
 
-  feed.items.forEach(item => {
-    console.log(item.title + ':' + item.link)
-  });
+  // create array of promises.
+  // I want to put into a try catch block but not sure how.
+  const feedPromises = feedURls.map( async (fds) => {
+    const returnedFeeds = await parser.parseURL(fds);
+    return returnedFeeds;
+  })
 
-})();
+  // now we wait for the array of promises to get resolved
+  const allFeeds = await Promise.all(feedPromises);
+  console.log( allFeeds ) 
+
+}
+
+feedsLoop()
+
 
