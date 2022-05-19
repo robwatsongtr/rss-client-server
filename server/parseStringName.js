b9bab611071eb4744de174bc07b47906376c44e8
robwatsongtr <rwatso@gmail.com>
2022-05-19 14:20:35 -0700

    parseStringName.js

---
+++ server/parseStringName.js
@@ -0,0 +1,21 @@
+// Program to parse a string into names
+
+/*
+The approach, scan the string for a comma then space, or a comma, space, 
+and and and split there
+
+
+
+*/
+
+
+const splitStringNames = (str) => {
+
+
+
+}
+
+
+console.log( 
+  splitStringNames('Surya Mattu, Angie Waller, Simon Fondrie-Teitler, and Micha Gorelick ')
+)
